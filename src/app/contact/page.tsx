fb58079481a5753e8917bc5b1c8fc4fe4db5a146
matheusnff85 <matheus.marinhodsp@gmail.com>
2024-08-05 18:42:43 -0300

    feat: inclui metadata na pagina inicial

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import Link from "next/link";
 import { Navbar } from "../components/navbar";
 import { Card } from "../components/Card";
+import Link from "next/link";
 import { Mail, Github, Linkedin } from "lucide-react";
 
 const socials = [
@@ -27,7 +27,7 @@ const socials = [
 
 export default function Contact() {
   return (
-    <div className="bg-gradient-to-tl from-zinc-900/0 to-zinc-900/0">
+    <div>
       <Navbar />
       <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
         <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
